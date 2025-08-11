2a6530f58736d0c664c238fcb82299026596769e
goentvy <entvyv@gmail.com>
2025-08-12 08:40:03 +0900

    gh-pages add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/entvy_blog_admin/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
